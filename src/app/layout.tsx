16281c358e786fd79b5f4f452c94b5b2dbf8fce8
akudrinsky <kudrinsky.am@gmail.com>
2025-10-18 08:07:26 +0200

    Add Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import "katex/dist/katex.min.css";
 import NavBar from "@/components/NavBar";
@@ -42,6 +43,7 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+        <SpeedInsights />
       </body>
     </html>
   );
